af79ad323cf9c57a1c89fa452510ead99abfe8d6
Damian Tomlinson <secretchaff@gmail.com>
2021-08-31 09:49:04 -0400

    Made changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
 
   const add = () => setCount(count + 1);
-
-  const talk = () => setMessage(message + "Hello \n");
+  const subtract = () => setCount(count - 1);
 
   return (
     <div className="App">
-      <h1>{message}</h1>
       <h1>Count:{count}</h1>
       <button onClick={add} id="add">
         Add
       </button>
-      <button onClick={talk} id="talk">
-        Speak
+      <button onClick={subtract} id="subtract">
+        subtract
       </button>
     </div>
   );
