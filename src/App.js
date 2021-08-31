e2be487b3bac5d98c24516d5d18aebb03cebacea
Damian Tomlinson <secretchaff@gmail.com>
2021-08-31 01:32:00 -0400

    Practiced using useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [message, setMessage] = useState("");
+
+  const add = () => setCount(count + 1);
+
+  const talk = () => setMessage(message + "Hello \n");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{message}</h1>
+      <h1>Count:{count}</h1>
+      <button onClick={add} id="add">
+        Add
+      </button>
+      <button onClick={talk} id="talk">
+        Speak
+      </button>
     </div>
   );
 }
